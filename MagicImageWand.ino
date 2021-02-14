bd246782132b785542e6c54d495cd8e1a1e44f72
Martin Nohr <martin.nohr@nohrphoto.com>
2021-02-13 18:51:37 -0700

    pullups don't work on GPIO 35, removed the code.

--- MagicImageWand.ino
+++ MagicImageWand.ino
@@ -216,7 +216,7 @@ void setup()
 	gpio_set_direction(GPIO_NUM_0, GPIO_MODE_INPUT);
 	gpio_set_pull_mode(GPIO_NUM_0, GPIO_PULLUP_ONLY);
 	gpio_set_direction(GPIO_NUM_35, GPIO_MODE_INPUT);
-	gpio_set_pull_mode(GPIO_NUM_35, GPIO_PULLUP_ONLY);
+	//gpio_set_pull_mode(GPIO_NUM_35, GPIO_PULLUP_ONLY); // not needed since there are no pullups on 35, they are input only
 
 	DisplayCurrentFile();
 	/*
