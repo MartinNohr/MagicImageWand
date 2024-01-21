f57dc78a2cb8940c2ffaeccfce82ebd9662909f5
Martin Nohr <martin.nohr@nohrphoto.com>
2024-01-20 20:09:48 -0700

    Added code for S3 TTGO.

--- MIWconfig.h
+++ MIWconfig.h
@@ -1,21 +1,20 @@
 #pragma once
 // ***** Various switches for options are set here *****
 
-// use TTGO T 1 or 4, define only one
+// use TTGO T 1, 3 or 4, define only one
 #define TTGO_T 1
+//#define TTGO_T_S3 3
 //#define TTGO_T 4
 
 // also remember to change User_Setup_Select.h correctly
 // use one of these in that file
-//#include <User_Setups/Setup22_TTGO_T4_v1.3.h>      // Setup file for ESP32 and TTGO T4 version 1.3
 //#include <User_Setups/Setup25_TTGO_T_Display.h>    // Setup file for ESP32 and TTGO T-Display ST7789V SPI bus TFT
+//#include <User_Setups/Setup206_LilyGo_T_Display_S3.h>     // For the LilyGo T-Display S3 based ESP32S3 with ST7789 170 x 320 TFT
+// make sure this one is commented out
+// //#include <User_Setup.h>           // Default setup is root library folder
 
 // 1 for standard SD library, 0 for the new exFat library which allows > 32GB SD cards
 #define USE_STANDARD_SD 0
-// reverse A and B for some PCB or wired versions, this is set for rev 2 PCB, 0 for older PCB, and 0 for rev 3 pcb
-#define ROTARY_DIAL_REVERSE 0
-// The push button setting, set to 1 for onboard PS version 1.4
-#define PUSH_BUTTON_PORT 0
 
 #if TTGO_T == 1
 	// SD details
@@ -34,20 +33,36 @@
 	#define BATTERY_SENSOR_GPIO 36
 	// the optional light sensor GPIO to control the LCD brightness
 	#define LIGHT_SENSOR_GPIO 39
-	// set the push button GPIO port
-	#if PUSH_BUTTON_PORT
+	// The push button setting, set to 1 for onboard PS version 1.4 (NOTE: PCB not ever created)
+#if 0
 		#define DIAL_BTN 37
 	#else
 		#define DIAL_BTN 15
 	#endif
 	// default dial direction GPIO ports
-	#if ROTARY_DIAL_REVERSE
-		#define DIAL_A 12
-		#define DIAL_B 13
-	#else
-		#define DIAL_A 13
-		#define DIAL_B 12
-	#endif
+	#define DIAL_A 13
+	#define DIAL_B 12
+#elif TTGO_T == 3
+	// SD details
+	#define SDcsPin    10
+	#define SDSckPin   12
+	#define SDMisoPin  11
+	#define SDMosiPin  13
+	// LED controller pins
+	#define DATA_PIN1 21
+	#define DATA_PIN2 16
+	// battery level
+	#define HAS_BATTERY_LEVEL 1
+	// the wheel frame advance gpio port
+	#define FRAMEBUTTON_GPIO 18
+	// battery sensor GPIO
+	#define BATTERY_SENSOR_GPIO 17
+	// the optional light sensor GPIO to control the LCD brightness
+	#define LIGHT_SENSOR_GPIO 18
+	// set the push button GPIO port
+	#define DIAL_BTN 3
+	#define DIAL_A 1
+	#define DIAL_B 2
 #elif TTGO_T == 4
 	// SD details
 	#define SDcsPin    13
